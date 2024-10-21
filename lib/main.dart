72bff612585ff67dc495d9637b0978125b86f7c1
Juan Manuel Carranza <ikkicarranza@gmail.com>
2024-10-21 11:57:45 -0400

    Add diferent types of buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/router/app_router.dart';
 import 'package:widgets_app/config/theme/app_theme.dart';
-import 'package:widgets_app/presentation/screens/buttons/buttosn_screen.dart';
-import 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
-import 'package:widgets_app/presentation/screens/home/home_screen.dart';
+// import 'package:widgets_app/presentation/screens/buttons/buttosn_screen.dart';
+// import 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
+// import 'package:widgets_app/presentation/screens/home/home_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -16,7 +16,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      theme: AppTheme(selectedColor: 3).getTheme(),
+      theme: AppTheme(selectedColor: 0).getTheme(),
       routerConfig: appRouter,
       );
   }
--- lib/presentation/screens/buttons/buttosn_screen.dart
+++ lib/presentation/screens/buttons/buttosn_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:go_router/go_router.dart';
 class ButtosnScreen extends StatelessWidget {
   static const String name= 'buttons_screen';
   const ButtosnScreen({super.key});
@@ -10,7 +10,53 @@ class ButtosnScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Buttons Screen"),
       ),
-      body: const Placeholder(),
+      body: const _buttonsView(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          context.pop();
+        },
+        child: Icon( Icons.arrow_back_ios_new_outlined ),
+        ),
+    );
+  }
+}
+
+class _buttonsView extends StatelessWidget {
+  const _buttonsView();
+
+  @override
+  Widget build(BuildContext context) {
+    return  SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+        child: Wrap(
+          spacing: 10,
+          alignment: WrapAlignment.center,
+          children: [
+            ElevatedButton(onPressed: () {} , child: const Text("Elevet")), 
+            const ElevatedButton(onPressed: null , child: Text("Elevet Disabled")), 
+            ElevatedButton.icon(
+              onPressed: (){}, 
+              label: const Text('Elevated Icon'),
+              icon: const Icon( Icons.access_alarm_rounded)), 
+              FilledButton(onPressed: (){}, child: const Text('Filled')),
+              FilledButton.icon(
+                onPressed: (){}, 
+                icon: const Icon(Icons.account_balance_wallet_rounded),
+                label: const Text('Filled Icon'),
+                ),
+                OutlinedButton(onPressed: (){}, child: const Text("outline")),
+                OutlinedButton.icon(onPressed: (){}, label: const Text("outline Icon"), icon: const Icon(Icons.access_alarms_outlined)),
+                TextButton(onPressed: (){}, child: const Text('text')),
+                TextButton.icon(onPressed: (){}, label: const Text('Text Icon'), icon: const Icon(Icons.airline_seat_flat_angled_outlined),), 
+                //TODO Custom Button
+                IconButton(onPressed: (){}, icon: const Icon(Icons.add_home_work_rounded)), 
+                 
+                
+          ]
+        )
+      ),
     );
   }
 }
